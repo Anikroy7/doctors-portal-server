ef65830f0e28955146d1b59b437e2fe09dee983c
Anikroy7 <roy645800@gmail.com>
2022-05-17 16:08:00 +0600

    user api created with jwt token

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 const cors = require('cors');
+var jwt = require('jsonwebtoken');
+const { response } = require('express');
 const port = process.env.PORT || 5000
 
 // middleware 
@@ -22,11 +24,74 @@ async function run() {
     try {
         await client.connect()
         const serviceCollection = client.db('doctors_portal').collection('services');
+        const bookingCollection = client.db('doctors_portal').collection('bookings');
+        const userCollection = client.db('doctors_portal').collection('user');
         app.get('/service', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query)
             const result = await cursor.toArray()
             res.send(result)
+        })
+
+        app.get('/available', async (req, res) => {
+            const date = req.query.date || 'May 16, 2022'
+
+            const services = await serviceCollection.find().toArray()
+
+            const query = { date: date }
+
+            const bookings = await bookingCollection.find(query).toArray();
+
+            services.forEach(service => {
+                const serviceBookings = bookings.filter(b => b.treatement === service.name)
+                const booked = serviceBookings.map(s => s.slot)
+                service.booked = booked;
+                const available = service.slots.filter(slot => !booked.includes(slot))
+                service.slots = available;
+            })
+
+            res.send(services)
+        })
+
+        app.get('/booking', async (req, res) => {
+            const patient = req.query.patient;
+            const query = { patient }
+            const bookings = await bookingCollection.find(query).toArray();
+            res.send(bookings)
+        })
+
+
+        app.put('/user/:email', async (req, res) => {
+
+            const email = req.params.email;
+            const filter = { email: email }
+            const options = { upsert: true };
+            const user = req.body;
+            const updateDoc = {
+                $set: user,
+            }
+            const result = await userCollection.updateOne(filter, updateDoc, options)
+            const token = jwt.sign({ email: email }, process.env.SECRET_ACCSESS_TOKEN, {
+                expiresIn: '1h'
+            })
+            res.send({ result, token })
+
+        })
+
+        app.post('/booking', async (req, res) => {
+            const booking = req.body;
+            console.log(booking.treatement);
+            const query = { treatement: booking.treatement, date: booking.date, patient: booking.patient }
+            console.log(query);
+            const exists = await bookingCollection.findOne(query);
+            if (exists) {
+                return res.send({ success: false, booking: exists })
+            }
+            else {
+                const result = await bookingCollection.insertOne(booking);
+                return res.send({ success: true, result })
+            }
+
         })
     }
     finally {
